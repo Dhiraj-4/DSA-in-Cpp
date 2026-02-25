79d85294b585dbea09d7f58870aea15668e2c5c6
Dhiraj-4 <dhirajdipaklondhe4@gmail.com>
2026-02-25 21:48:21 +0530

    learning sliding window

--- sliding window/firstNegOfEveryWindowK.cpp
+++ sliding window/firstNegOfEveryWindowK.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <deque>
 using namespace std;
 
     // brute force
@@ -22,33 +23,55 @@ using namespace std;
     //     return ans;
     // }
 
+
+    // better then previous solution
+    // vector<int> firstNegOfEveryWindow(vector<int>& nums, int k) {
+    //     int n = nums.size();
+
+    //     vector<int> negPositions;
+
+    //     for(int i = 0; i < n; i++) {
+    //         if(nums[i] < 0) negPositions.push_back(i);
+    //     }
+
+    //     vector<int> ans;
+    //     int m = negPositions.size();
+
+    //     int idx = 0;
+    //     int i = 0;
+    //     int j = k-1;
+        
+    //     while(j < n) {
+
+    //         while(idx < m && negPositions[idx] < i) idx++;
+
+    //         if(idx < m && (i <= negPositions[idx] && negPositions[idx] <= j)) 
+    //             ans.push_back(nums[negPositions[idx]]);
+    //         else ans.push_back(0);
+
+    //         i++;
+    //         j++;
+    //     }
+
+    //     return ans;
+    // }
+
+
     vector<int> firstNegOfEveryWindow(vector<int>& nums, int k) {
-        
-        //                       i      j
-        // 0   1  2  3   4   5  6   7  8            k
-        // 2, -3, 4, 4, -7, -1, 4, -2, 6  [1, 4, 5, 7]
-
-        // ans -3 -3 -7 -7 -1 -2
-        // -3 -3 -7 -7 -7 -1 -2
-
         int n = nums.size();
+        vector<int> ans(n-k+1);
 
-        vector<int> negPositions;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] < 0) negPositions.push_back(i);
-        }
-        
-        vector<int> ans;
-
-        int idx = 0;
+        int p = -1;
         int i = 0;
         int j = k-1;
-        
+
         while(j < n) {
-            if(negPositions[idx] < i) idx++;
-            else if(i <= negPositions[idx] && negPositions[idx] <= j) 
-                ans.push_back(nums[negPositions[idx]]);
+            if(p < i) p = i;
+
+            while(p <= j && nums[p] >= 0) p++;
+
+            if(p <= j) ans[i] = nums[p];
+            else ans[i] = 0;
 
             i++;
             j++;
@@ -56,17 +79,20 @@ using namespace std;
 
         return ans;
     }
+    
 
     int main() { 
-        //                      m
+        //                  m
         // negPositions = 1 4 5 7
 
         // -3 -3 -7 -7 -7 -1 -2
-        //                                            i      j
+        //                          i      j
         //                   0   1  2  3   4   5  6   7  8
         vector<int> nums = { 2, -3, 4, 4, -7, -1, 4, -2, 6 }; // 1 <= n <= 10^5
                         // -3 -3 -7 -7 -7 -1 -2
-                // nums = { 2, 3, 4, 5, 6, -2, 4, 4, 6, -1, -4, 3, 4 };
+                nums = { 2, 3, 4, 5, 6, -2, 4, 4, 6, -1, -4, 3, 4 };
+
+                // nums = {1, 2, 3, 4, 5};
                 // -2 -2 -2 -1 -1 -1 -4 
     int k = 3; // 1 <= k <= n;
 
